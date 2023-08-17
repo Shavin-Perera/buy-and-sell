34f2f0cbfde1a6f383383fe44dd58f740aa2c1a8
Shavin Perera <rmshavinperera@gmail.com>
2023-08-18 00:54:22 +0530

    listing-details-completed

---
+++ src/app/fake-data.ts
@@ -0,0 +1,18 @@
+import { Listing } from './types';
+
+export const fakeListings: Listing[] = [{
+    id: '123',
+    name: 'Old Boat',
+    description: 'A very old boat. Bargain price',
+    price: 700,
+}, {
+    id: '345',
+    name: 'Computer',
+    description: 'From the 1990s, a classic!',
+    price: 50,
+}, {
+    id: '456',
+    name: 'Basketball Hoop',
+    description: 'Good condition, free delivery',
+    price: 100,
+}];
--- src/app/listing-details/listing-details.component.ts
+++ src/app/listing-details/listing-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
-  selector: 'app-listing-details',
+  selector: 'app-listing-detail-page',
   templateUrl: './listing-details.component.html',
   styleUrls: ['./listing-details.component.css']
 })
-export class ListingDetailsComponent {
+export class ListingDetailsComponent implements OnInit {
+  listing: Listing | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+  }
 
 }
